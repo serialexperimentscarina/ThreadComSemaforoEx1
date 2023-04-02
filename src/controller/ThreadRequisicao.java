6378fbdbc058a0cf69372e48cc498794d426e28a
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-04-02 16:20:53 -0300

    feat: ThreadRequisicao

--- src/controller/ThreadRequisicao.java
+++ src/controller/ThreadRequisicao.java
@@ -1,5 +1,61 @@
 package controller;
 
-public class ThreadRequisicao {
+import java.util.concurrent.Semaphore;
+
+public class ThreadRequisicao extends Thread {
+	
+	private Semaphore mutex;
+	private int threadID;
+	
+	public ThreadRequisicao(Semaphore mutex, int threadID) {
+		this.mutex = mutex;
+		this.threadID = threadID;
+	}
+	
+	@Override
+	public void run() {
+		if (threadID % 3 == 1) {
+			executaRequisicao(2, 200, 1000, 1000);	
+		} else if(threadID % 3 == 2) {
+			executaRequisicao(3, 500, 1500, 1500);
+		} else {
+			executaRequisicao(3, 1000, 2000, 1500);
+		}
+	}
+	
+	private void executaRequisicao(int repeticoes, int tempoMinCalculo, int tempoMaxCalculo, int tempoBD) {
+		for (int i = 0; i < repeticoes; i++) {
+			int tempoCalculo = (int)((Math.random() * (tempoMaxCalculo - tempoMinCalculo + 1)) + tempoMinCalculo);
+			calculos(tempoCalculo, (i * 2) + 1, (repeticoes * 2));
+			try {
+				mutex.acquire();
+				transacaoBD(tempoBD, (i * 2) + 2, (repeticoes * 2));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				mutex.release();
+			}
+		}
+		System.out.println("A requisicao #" + threadID + " terminou de ser processada.");
+	}
+	
+	private void transacaoBD(int tempoTransacao, int passoAtual, int passosTotais) {
+		try {
+			sleep(tempoTransacao);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("A requisicao #" + threadID + " terminou de realizar uma transacao em BD (Passo " + passoAtual + "/" + passosTotais + ")");
+	}
+
+	private void calculos(int tempoCalculo, int passoAtual, int passosTotais) {
+		try {
+			sleep(tempoCalculo);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("A requisicao #" + threadID + " terminou de realizar calculos (Passo " + passoAtual + "/" + passosTotais + ")");
+
+	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,8 +1,19 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadRequisicao;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		Semaphore mutex = new Semaphore(1);
+		
+		for (int i = 1; i <= 21; i++) {
+			Thread requisicao = new ThreadRequisicao(mutex, i);
+			requisicao.start();
+		}
 
 	}
 
